79ea0bef71c9e6eb206f036fd6017e314c8c933c
nemo.zhong <zyj724@gmail.com>
2013-06-06 18:30:09 +0800

    init sonclub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonclub::Application.config.secret_token = '4d30f2cc90af4ad664a79f75f3578adc475cb83b8b3c4ce9e85636786217ebd385d96cfcaa16adcac1c5034cff72d747638317163cd8c58947ddf606b9ec97ec'
