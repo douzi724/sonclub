0349905f156b87879bdbba2b8054398512e113e1
nemo <zyj724@gmail.com>
2013-06-07 22:57:36 +0800

    add weixin auth

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Sonclub
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # 微信 API token
+    config.weixin_token = "nemo"
   end
 end
